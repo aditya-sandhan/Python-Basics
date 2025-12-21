558c60f9c437f17e27548d6a90623429b1cbf337
Aditya Sandhan <adityasandhan.ai@gmail.com>
2025-12-21 22:49:44 +0530

    Create If-Else.py
    applying If-else conditions.

---
+++ If-Else.py
@@ -0,0 +1,8 @@
+# Checking whether the following member is able to vote and drive 
+age = int(input("Enter your Age: "))
+
+if (age >= 18):
+    print("Member is eligible to vote and drive!")
+else:
+    print("Member is NOT eligible to vote and drive!!")
+    

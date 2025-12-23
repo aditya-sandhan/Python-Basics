b3aaad4018808ee21ddc13dff843b283d0bcd189
Aditya Sandhan <adityasandhan.ai@gmail.com>
2025-12-23 23:00:34 +0530

    Add list accessing and slicing demonstrations

---
+++ String,List,Tuple,Dict,Set/Lists.py
@@ -0,0 +1,12 @@
+# Demonstration of list accessing 
+list1=[10,20,30,40,50]
+
+print(list1[0])  # Accessing first element
+print(list1[2])  # Accessing third element
+
+# Demonstration of list slicing
+marks = [10,15,17,21,"abc",100.99]
+
+print(marks[:3])
+print(marks[2:5])
+print(marks[1:])

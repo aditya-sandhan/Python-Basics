f75f31e76cfe753d1b85f806c2987e5f45edd77b
Aditya Sandhan <adityasandhan.ai@gmail.com>
2025-12-24 00:10:19 +0530

    Linear Search using For Loop

---
+++ String,List,Tuple,Dict,Set/For-Loop-List.py
@@ -0,0 +1,12 @@
+# Linear Search using For Loop
+
+list1 = [1,3,4,2,10,11,4] 
+
+idx=0
+for i in list1:
+    
+    if (i == 10):   
+        print(f"The index of 10 is: {idx}")
+        break
+    idx += 1
+        

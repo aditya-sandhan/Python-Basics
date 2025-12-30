914d716bba6fa963229bf9055eb682210fe9a4f1
Aditya Sandhan <adityasandhan.ai@gmail.com>
2025-12-30 12:39:06 +0530

    updated some more info

--- String,List,Tuple,Dict,Set/Dict.py
+++ String,List,Tuple,Dict,Set/Dict.py
@@ -1,9 +1,14 @@
-# Dictionary in Python
+# Dictionary in Python - Mutable and Unordered collection of key-value pairs
 
 dict1 = {
     "name" : "Aditya Sandhan",
     "Marks" : [10,15,12],
-    "Age": 20           #
+    "Age": 20           
 }
 
-print(dict1)        
+print(dict1)  
+print(type(dict1))      
+
+dict1["Age"] = 24   # Updating Age
+dict1["City"] = "Pune"  # Adding new key-value pair
+print(dict1)

b5d8a615c70bd4a7944da485c400242384859a5e
Aditya Sandhan <adityasandhan.ai@gmail.com>
2025-12-30 14:10:15 +0530

    Set implementation

---
+++ String,List,Tuple,Dict,Set/sets-methods.py
@@ -0,0 +1,11 @@
+s = {1,2,3}
+
+s.add(4)  # Adding single element
+print(s)
+s.remove(2)  # Removing element
+print(s)
+s.update([5,6,7])  # Adding multiple elements
+print(s)
+
+s.clear()  # Removing all elements
+print(s) # Output: set() - empty set
---
+++ String,List,Tuple,Dict,Set/setsEg.py
@@ -0,0 +1,7 @@
+mysets = {"apple", "banana", "cherry"}
+print(mysets)
+print(type(mysets))
+
+mysets2 = {1,2,3,2,2,2}
+print(mysets2)  # Duplicates will be removed - one copy only
+print(len(mysets2)) # Length will be 3

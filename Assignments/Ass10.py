969e8642831ba09b20a7a88653d73e02abc9b2a8
Aditya Sandhan <adityasandhan.ai@gmail.com>
2026-01-06 22:51:29 +0530

    assess

--- Assignments/Ass10.py
+++ Assignments/Ass10.py
@@ -1,7 +1,19 @@
-str1= input("Enter the digits: ")
-str2= input("Enter the digits: ")
+''' Input two list from the user. Merge them into one result and Sort them'''
 
-list1= list(str1)
-list2= list(str2)
+list1 =[]
 
-print(list1 + list2)
+for i in range(1,4) :
+    q = int(input(f"Enter digits {i}: "))
+    list1.append(q)
+    
+print(list1)
+list2 =[]
+
+for i in range(4,7) :
+    r = int(input(f"Enter digits {i}: "))
+    list2.append(r)
+    
+print(list2)
+list3=list1+list2
+list3.sort()
+print(list3)

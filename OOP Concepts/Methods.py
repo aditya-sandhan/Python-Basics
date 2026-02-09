42cd4caad7d0052b23f7896dd44e4c96a54fc645
Aditya Sandhan <adityasandhan.ai@gmail.com>
2026-02-09 21:16:13 +0530

    Methods

--- OOP Concepts/Methods.py
+++ OOP Concepts/Methods.py
@@ -13,11 +13,14 @@ class Laptop:
     def get_info(self):     # Instance Method - uses Instance attributes 
         print(f"Laptop has {self.RAM} RAM & {self.Storage} {self.storage_type}")# We can also access Class attribute by self 
 
-l1 = Laptop("16gb","1tb")
-l2 = Laptop("8gb", "256gb")
+    @staticmethod
+    def cal_discount(price,discount):
+        final_price = price - (discount * price /100)
+        print(f"Discounted price = {final_price}")
 
+l1 = Laptop("16gb","1tb")
 l1.get_info()
-l2.get_info()
+
 Laptop.get_storage_type()    # Class method can be accessed by Class as well as Objects too
 l1.get_storage_type()       # Here its accessed by object
 
